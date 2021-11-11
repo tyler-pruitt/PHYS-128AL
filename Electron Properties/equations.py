60cc85bfb4b53a110f45fe8cf919d5e4d57eaaed
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-11-10 16:04:36 -0800

    Add files via upload

--- Electron Properties/equations.py
+++ Electron Properties/equations.py
@@ -8,57 +8,43 @@ Created on Mon Nov  1 15:06:20 2021
 
 import numpy as np
 
-def chargeOfDrop(fallVelocity, riseVelocity, a=0):
+def chargeOfDrop(fallVelocity, riseVelocity):
     V = 500
     rho = 886
-    n = 1.8285 * 10**(-5)
+    n = 1.831 * 10**(-5)
     p = 101325
     b = 8.2 * 10**(-3)
     g = 9.80665
     d = 0.00914
     
-    if a == 0:
-        q = (4 * np.pi / 3) * (np.sqrt((b / (2*p))**2 + ((9 * n * fallVelocity) / (2 * rho * g)) ) - (b / (2 * p)) )**3 * ((rho * g * d * (fallVelocity + riseVelocity)) / (V * fallVelocity))
-    else:
-        q = (4 * np.pi / 3) * a**3 * ((rho * g * d * (fallVelocity + riseVelocity)) / (V * fallVelocity))
+    q = (4 * np.pi / 3) * (np.sqrt((b / (2*p))**2 + ((9 * n * fallVelocity) / (2 * rho * g)) ) - (b / (2 * p)) )**3 * ((rho * g * d * (fallVelocity + riseVelocity)) / (V * fallVelocity))
     
-    return q
-
-def radiusOfDrop(fallVelocity):
-    rho = 886
-    n = 1.8285 * 10**(-5)
-    p = 101325
-    b = 8.2 * 10**(-3)
-    g = 9.80665
-    
-    a = np.sqrt((b / (2*p))**2 + ((9 * n * fallVelocity) / (2 * rho * g)) ) - (b / (2 * p))
-    
-    return a
-
-def chargeError(dVf, dVr, Vf, Vr, dV, dn, dd):
-    V = 500
-    rho = 886
-    n = 1.8285 * 10**(-5)
-    g = 9.80665
-    d = 0.00914
-    
-    factor = (4 * np.pi * g * rho / 3)
-    
-    dq = 1 * np.sqrt( ((dVf + dVr) / (Vf + Vr))**2 + (dd / d)**2 + (dV / V)**2 + (745 / 16) * (dVf / Vf)**2 + (729 / 16) * (dn / n)**2 )
-    
-    return dq
-
-response = input("To end enter 'end', else press enter: ")
-
-while response != "end":
-    Vrise = float(input("Enter Vrise: "))
-    Vfall = float(input("Enter Vfall: "))
-    
-    q = chargeOfDrop(Vfall, Vrise)
     e = 1.60217662 * 10**(-19)
     
-    print("Charge:", q, "C")
-    print("Charge:", q/e, "e")
+    return q, q / e
+
+def chargeError(dVf, dVr, Vf, Vr):
+    V = 500
+    dV = 10
+    dn = 0.003 * 10**(-5)
+    rho = 886
+    n = 1.831 * 10**(-5)
+    g = 9.80665
+    d = 0.00914
+    b = 8.2 * 10**(-3)
+    p = 101325
     
-    response = input("To end enter 'end', else press enter: ")
+    A = (4 * np.pi * g * rho / 3)
+    B = (b / (2 * p))**2 + 9 * n * Vf / (2 * rho * g)
+    C = b / (2 * p)
+    D = d * (Vf + Vr) / (V * Vf)
+    F = B**(1/2) - C
+    q = A * (F)**3 * D
+    dB = B * ((dn/n)**2 + (dVf/Vf)**2)**2
+    
+    dD = D * (((dVf + dVr) / (Vf + Vr))**2 + (dV / V)**2 +(dVf / Vf)**2)**(1/2)
+    
+    dq = q * (9 * (dB / (2 * (B - C * B**(1/2))))**2 + (dD / D)**2)**(1/2)
+    
+    return dq
 
